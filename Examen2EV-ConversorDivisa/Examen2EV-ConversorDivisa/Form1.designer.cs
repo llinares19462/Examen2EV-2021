884d74121a46ff2bb0751a2221f0f6f5922f2691
llinares19462 <llinares19462@iesmarenostrum.com>
2021-03-16 19:54:20 +0100

    REalizada depuración segun requerimientos del enunciado.

--- Examen2EV-ConversorDivisa/Examen2EV-ConversorDivisa/Form1.designer.cs
+++ Examen2EV-ConversorDivisa/Examen2EV-ConversorDivisa/Form1.designer.cs
@@ -56,6 +56,7 @@
             this.tImporte.Name = "tImporte";
             this.tImporte.Size = new System.Drawing.Size(103, 38);
             this.tImporte.TabIndex = 2;
+
             // 
             // tResultado
             // 
